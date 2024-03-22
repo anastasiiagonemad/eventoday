52725a9d36d340415f6e802f0c1b7efc5a9ab858
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-03-22 15:56:50 +0300

    add responsive phone vers

--- owl/owl.js
+++ owl/owl.js
@@ -1,22 +1,70 @@
 $(document).ready(function () {
   $('#carouselOne').owlCarousel({
-    loop: true, //Зацикливаем слайдер
-    margin: 20, //Отступ от элемента справа в 50px
-    nav: true, //Отключение навигации
-    autoplay: true, //Автозапуск слайдера
-    smartSpeed: 1000, //Время движения слайда
-    autoplayTimeout: 2000, //Время смены слайда
+    loop: true,
+    margin: 20,
+    nav: true,
+    autoplay: false,
     dots: false,
     responsive: {
-      //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+      //Адаптивность.
       0: {
         items: 1,
       },
       600: {
-        items: 2,
+        items: 1,
+      },
+      800: {
+        items: 1,
       },
       1000: {
         items: 2,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $('#carouselTwo').owlCarousel({
+    loop: true,
+    margin: 30,
+    center: true,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 3000,
+    responsive: {
+      //Адаптивность.
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $('#carouselThree').owlCarousel({
+    cenetr: true,
+    loop: true,
+    margin: 20,
+    nav: false,
+    autoplay: false,
+    dots: false,
+    mouseGrag: true,
+    touchDrag: true,
+    responsive: {
+      //Адаптивность.
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
       },
     },
   });
