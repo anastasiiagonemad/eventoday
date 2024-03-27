6fe12b0d7e0d9df89bd7e793c48107b56d457fec
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-03-27 15:36:17 +0300

    create partner.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//timer for buytickets.html
 document.addEventListener('DOMContentLoaded', function () {
   const newLocal = 4;
   const newLocal_1 = 1;
@@ -47,4 +48,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
   countdownTimer();
   timerId = setInterval(countdownTimer, 1000);
+});
+
+//open sposor description in sronsor.html
+const open1 = document.querySelector('.open1');
+const open2 = document.querySelector('.open2');
+const open3 = document.querySelector('.open3');
+const close = document.querySelectorAll('.close');
+const text1 = document.querySelector('.text1');
+const text2 = document.querySelector('.text2');
+const text3 = document.querySelector('.text3');
+
+open1.addEventListener('click', () => {
+  text1.classList.add('active');
+  open1.style.display = 'none';
+});
+
+open2.addEventListener('click', () => {
+  text2.classList.add('active');
+  open2.style.display = 'none';
+});
+
+open3.addEventListener('click', () => {
+  text3.classList.add('active');
+  open3.style.display = 'none';
+});
+
+close.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    text1.classList.remove('active');
+    text2.classList.remove('active');
+    text3.classList.remove('active');
+    open1.style.display = 'block';
+    open2.style.display = 'block';
+    open3.style.display = 'block';
+  });
 });
