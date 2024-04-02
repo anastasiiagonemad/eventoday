791e2b59f24e052791262ab762a9a89b9baa7c54
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-04-02 23:23:32 +0300

    add select dropdown

--- js/archive.js
+++ js/archive.js
@@ -16,3 +16,46 @@ document.addEventListener('DOMContentLoaded', () => {
     modal.style.display = 'none';
   });
 });
+
+//select-dropdown
+function showDropdown(id) {
+  const dropdown = document.getElementById(id);
+  if (dropdown.style.display === 'none') {
+    dropdown.style.display = 'block';
+  } else {
+    dropdown.style.display = 'none';
+  }
+}
+
+// Добавление обработчика событий на клик по родительскому элементу
+document
+  .querySelector('.select-dropdown1')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('selectDropdown1');
+    showDropdown('selectDropdown1');
+  });
+
+document
+  .querySelector('.select-dropdown2')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('selectDropdown2');
+    showDropdown('selectDropdown2');
+  });
+
+document
+  .querySelector('.select-dropdown3')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('selectDropdown3');
+    showDropdown('selectDropdown3');
+  });
+
+document
+  .querySelector('.select-dropdown4')
+  .addEventListener('click', function (event) {
+    event.stopPropagation();
+    const dropdownContent = document.getElementById('selectDropdown4');
+    showDropdown('selectDropdown4');
+  });
